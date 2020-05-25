a88c57c6682a168fe980efcd3cacf1ff40b0c090
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-25 12:34:34 +0100

    create script for films' searching

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd71db054',
+            s: searchTerm,
+        }
+    });
+    console.log(response.data);
