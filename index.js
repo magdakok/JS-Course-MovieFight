c24662f8fc9e38ef749301d5bf72908c45b32b17
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-25 12:36:08 +0100

    add timeout that sends request just after stop typing

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ const fetchData = async (searchTerm) => {
         }
     });
     console.log(response.data);
+}
+
+let timeoutId;
+const input = document.querySelector('input');
+
+// //we have to divide it into two:
+// input.addEventListener('input', (event)=> {
+//     fetchData(event.target.value);
+// });
+
+const onInput = event => {
+    //data is fetched after one second, but timer turns off if input value changes between than one second -> effect: less unnecessary requests
+    if (timeoutId) {
+        clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(()=>{
+        fetchData(event.target.value);
+    },1000);
+}
+input.addEventListener('input', onInput);
