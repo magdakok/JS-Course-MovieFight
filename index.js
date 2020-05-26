7da7a7b711aebb8d1616785dc0ac60d30789e1d1
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-26 12:00:44 +0100

    refactor the movie script

--- index.js
+++ index.js
@@ -12,23 +12,42 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 }
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results">
+            
+            </div>
+        </div>
+    </div>
+
+`;
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+
+    resultsWrapper.innerHTML = '';
+
+    dropdown.classList.add('is-active');
+    for(let movie of movies) {
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+        option.classList.add('dropdown-item');
+        option.innerHTML =`
+            <img src="${imgSrc}"/>
+            ${movie.Title}
+        `;
+        resultsWrapper.appendChild(option);
+    }
+    
+}
 
 //DEBOUNCING AN INPUT - wait for some time to pass after the last event to actually run a function
-const onInput = async event => {
-        const movies = await fetchData(event.target.value);
-        printData(movies);
-}
 input.addEventListener('input', debounce(onInput,1000));
-
-const printData = (movies) => {
-    for (let movie of movies) {
-        const div = document.createElement('div');
-        div.innerHTML = `
-            <img src="${movie.Poster}" class="image"/>
-            <h1>${movie.Title}</h1>
-            <h2>(${movie.Year})</h2>
-        `;
-        document.querySelector('#searchResults').appendChild(div);
-    };
-};
